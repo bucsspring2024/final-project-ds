e494e9f16859d297aa2af5eabc0c2b8fec0722a9
Andrew Li <ali209@binghamton.edu>
2024-05-02 14:12:42 -0400

    Final Project Submission

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import pygame
-from controller import Controller
-#import your controller
+from src.controller import Controller
 
 def main():
     pygame.init() 
     start = Controller()
     start.mainloop()
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
--- src/sprite.py
+++ src/sprite.py
@@ -1,7 +1,15 @@
 import pygame
 
 class Sprite(pygame.sprite.Sprite): # Cookie is a subclass/child class of Sprite
-  def __init__(self, x, y, img = "./../assets/start.png", scale = 1.0):
+  def __init__(self, x, y, img = "assets/start.png", scale = 1.0):
+    """This 
+
+    Args:
+        x (int): This stores the x coordinate of top left corner of the sprite rectangle.
+        y (int): This stores the y coordinate of top left corner of the sprite rectangle.
+        img (str, optional): This stores a sprite image from the assets folder. Defaults to "assets/start.png".
+        scale (float, optional): This stores a scale for the size of the game sprite. Defaults to 1.0.
+    """
     super().__init__()
     self.image = pygame.image.load(img)
     self.width = self.image.get_width()
